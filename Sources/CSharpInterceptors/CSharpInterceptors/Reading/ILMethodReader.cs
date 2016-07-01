21f55c1c0e680ee8dc78f1d0b077dfce5abc4381
Stanislas Mur <stanislas.mur@gmail.com>
2016-07-01 23:03:32 +0200

    started creating MethodReader implementation based on ILReader

---
+++ Sources/CSharpInterceptors/CSharpInterceptors/Reading/ILMethodReader.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ILReaderLibrary.ILReader;
+using System.Reflection;
+using System.Collections;
+
+namespace CSharpInterceptors.Reading
+{
+    public class ILMethodReader : MethodReader
+    {
+        ILReader m_Reader;
+
+        private ILMethodReader() { }
+
+        public Instruction[] ReadMethod(MethodInfo method)
+        {
+            m_Reader = new ILReader(method);
+            IEnumerator<ILInstruction> enumerator = m_Reader.GetEnumerator();
+            List<Instruction> instructions = new List<Instruction>();
+            for(; enumerator.MoveNext();)
+            {
+                ILInstruction instruction = enumerator.Current;
+            }
+
+            return instructions.ToArray();
+        }
+    }
+}
