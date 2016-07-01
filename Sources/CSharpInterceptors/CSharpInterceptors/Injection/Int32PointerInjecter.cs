59ff54c6351f316e30361426012b97a7b8595802
Stanislas Mur <stanislas.mur@gmail.com>
2016-07-01 22:38:08 +0200

    for some reason Int32PointerInjecter was ignored from the git

---
+++ Sources/CSharpInterceptors/CSharpInterceptors/Injection/Int32PointerInjecter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpInterceptors.Creation;
+
+namespace CSharpInterceptors.Injection
+{
+    public class Int32PointerInjecter : AbstractPointerInjecter
+    {
+        public override void Inject(RuntimeMethodHandle injectHandle, RuntimeMethodHandle intoHandle)
+        {
+            unsafe
+            {
+                //*((IntPtr*)new IntPtr(((IntPtr*)intoHandle.Value.ToPointer() + 2)).ToPointer()) = injectHandle.GetFunctionPointer();
+
+                int* injected = (int*)injectHandle.Value.ToPointer() + 2;
+                int* target = (int*)intoHandle.Value.ToPointer() + 2;
+
+                *target = *injected;
+            }
+        }
+    }
+}
